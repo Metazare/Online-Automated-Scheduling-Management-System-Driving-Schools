7caa11038fe2c3287ec35e0f181da32b9e091545
Meeep <139204086+Meeepu@users.noreply.github.com>
2023-10-30 21:53:37 +0800

    fix: fix not using the dev values in envs

--- server/src/utilities/envs.ts
+++ server/src/utilities/envs.ts
@@ -1,13 +1,21 @@
-import { cleanEnv } from 'envalid';
+import { cleanEnv, makeValidator } from 'envalid';
 import { str, port } from 'envalid/dist/validators';
+
+const devDefault = (key: string, value: string) =>
+    makeValidator<string>((): string => {
+        const foundValue = process.env[key];
+        if (process.env.NODE === 'production' && foundValue) return foundValue;
+        return value;
+    })();
 
 export default cleanEnv(process.env, {
     NODE_ENV: str({ choices: ['development', 'production', 'test'] }),
     PORT: port({ default: 4000 }),
-    CORS_ORIGIN: str({ devDefault: 'http://localhost:3000/' }),
-    MONGO_URI: str({
-        devDefault: 'mongodb://127.0.0.1:27017/Online-Automated-Scheduling-Management-System-Driving-Schools'
-    }),
     JWT_ACCESS: str(),
-    JWT_REFRESH: str()
+    JWT_REFRESH: str(),
+    CORS_ORIGIN: devDefault('CORS_ORIGIN', 'http://localhost:3000/'),
+    MONGO_URI: devDefault(
+        'MONGO_URI',
+        'mongodb://127.0.0.1:27017/Online-Automated-Scheduling-Management-System-Driving-Schools'
+    )
 });
