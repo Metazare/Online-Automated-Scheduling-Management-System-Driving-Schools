2866d4b0ce877cd7e2eb7ee6c01a848e27405076
Harold Graphs <haroldcastillo272@gmail.com>
2023-11-05 00:21:17 +0800

    feat(Utilities): setup utilities for UI
    - install sass and MUI
    - setup MUI theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import Login from './Pages/Login'
+import Register from './Pages/Register'
 import Home from './Pages/Home';
 import BaseLayout from './Layouts/BaseLayout/BaseLayout';
 
+const theme = createTheme({
+  palette:{
+    primary: {
+      main:'#E24B5B',
+    },
+    secondary:{
+      main:'#2F2E5A',
+      dark:'#1C1B45'
+    }
+  }
+})
 function App() {
   return (
-    <Routes>
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
         <Route element={<BaseLayout />} >
-            <Route path="/" element={<Home/>} />
+          <Route path="/" element={<Home/>} />
         </Route>
-    </Routes>
+      </Routes>
+    </ThemeProvider>
+
   );
 }
 
