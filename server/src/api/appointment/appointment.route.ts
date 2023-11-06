9b0ca596e5114c831ead6b867df904d074420c42
Meeep <139204086+Meeepu@users.noreply.github.com>
2023-11-06 12:33:16 +0800

    docs: add comments for each endpoints' body/query

--- server/src/api/appointment/appointment.route.ts
+++ server/src/api/appointment/appointment.route.ts
@@ -6,10 +6,30 @@ import asynchronousHandler from '../../middlewares/asynchronousHandler';
 
 const router: Router = Router();
 
+/**
+ * appoinmentId (optional)
+ * enrollmentId (optional)
+ * instructorId (optional)
+ * status (optional)
+ */
 router.get('/', asynchronousHandler(getAppointments));
 
+/**
+ * enrollmentId
+ * instructorId
+ * vehicle
+ * schedule
+ */
 router.post('/', limitUsers(Role.ADMIN), asynchronousHandler(createAppointment));
 
+/**
+ * ADMIN (change schedule date)
+ * appointmentId
+ * schedule
+ * 
+ * STUDENT (request to reschedule)
+ * appointmentId
+ */
 router.patch('/', limitUsers(Role.ADMIN, Role.STUDENT), asynchronousHandler(updateAppointment));
 
 export default router;
--- server/src/api/enrollment/enrollment.route.ts
+++ server/src/api/enrollment/enrollment.route.ts
@@ -6,10 +6,29 @@ import asynchronousHandler from "../../middlewares/asynchronousHandler";
 
 const router = Router();
 
+/**
+ * enrollmentId (optional)
+ * courseId (optional)
+ * courseType (optional)
+ * status (optional)
+ */
 router.get('/', limitUsers(Role.ADMIN, Role.STUDENT), asynchronousHandler(getEnrollments));
 
+/**
+ * courseId
+ * days
+ * startTime
+ * endTime
+ */
 router.post('/', limitUsers(Role.STUDENT), asynchronousHandler(createEnrollment));
 
+/**
+ * enrollmentId
+ * status
+ * 
+ * [status === 'declined']
+ * reason
+ */
 router.patch('/', limitUsers(Role.ADMIN), asynchronousHandler(updateEnrollmentStatus));
 
 export default router;
