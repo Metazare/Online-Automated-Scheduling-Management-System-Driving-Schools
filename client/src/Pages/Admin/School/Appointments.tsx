7b90521455b44c6acaadcc771e6c5c70e0709edf
Harold Graphs <haroldcastillo272@gmail.com>
2023-11-06 22:58:33 +0800

    feat(Appointment Tab) : updated the input date time

--- client/src/Pages/Admin/School/Appointments.tsx
+++ client/src/Pages/Admin/School/Appointments.tsx
@@ -2,16 +2,13 @@ import React,{useState} from 'react'
 import Typography from '@mui/material/Typography'
 import { Grid,Button, Paper , Modal,Box, TextField,IconButton } from '@mui/material'
 import dayjs, { Dayjs } from 'dayjs';
-import Badge from '@mui/material/Badge';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { PickersDay, PickersDayProps } from '@mui/x-date-pickers/PickersDay';
-import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
-import { DayCalendarSkeleton } from '@mui/x-date-pickers/DayCalendarSkeleton';
 import AddIcon from '@mui/icons-material/Add';
 import MenuItem from '@mui/material/MenuItem';
-
-
+import TESTCalendar from '../../../Components/TESTCalendar';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 
 // TODO Calendar and resched Modal
 
@@ -31,92 +28,23 @@ const style = {
     p: 4,
 }; 
 
-function getRandomNumber(min: number, max: number) {
-    return Math.round(Math.random() * (max - min) + min);
-}
-function fakeFetch(date: Dayjs, { signal }: { signal: AbortSignal }) {
-    return new Promise<{ daysToHighlight: number[] }>((resolve, reject) => {
-        const timeout = setTimeout(() => {
-        const daysInMonth = date.daysInMonth();
-        const daysToHighlight = [1, 2, 3].map(() => getRandomNumber(1, daysInMonth));
-            resolve({ daysToHighlight });
-        }, 500);
-        signal.onabort = () => {
-            clearTimeout(timeout);
-            reject(new DOMException('aborted', 'AbortError'));
-        };
-    });
-}
-const initialValue = dayjs('2022-04-17');
-function ServerDay(props: PickersDayProps<Dayjs> & { highlightedDays?: number[] }) {
-    const { highlightedDays = [], day, outsideCurrentMonth, ...other } = props;
-    const isSelected = !props.outsideCurrentMonth && highlightedDays.indexOf(props.day.date()) >= 0;
-    return (
-        <Badge
-            key={props.day.toString()}
-            overlap="circular"
-            badgeContent={isSelected ? '🌚' : undefined}
-        >
-            <PickersDay {...other} outsideCurrentMonth={outsideCurrentMonth} day={day} />
-        </Badge>
-    );
-}
 function Appointments() {
 
-    // Calendar Controlled Value
-    const [value, setValue] = React.useState<Dayjs | null>(dayjs('2022-01-13'));
     // * Reason Value 
     const [reason,setReason] = useState("")
 
+    // * Declaration for adding new appointments form
     const [form,setForm] = useState({
         student:"",
         instructor:"",
         vehicle:"",
-        dateNtime:""
+        dateTime: dayjs('2022-04-17T15:30')
     })
-
-    const requestAbortController = React.useRef<AbortController | null>(null);
-    const [isLoading, setIsLoading] = React.useState(false);
-    const [highlightedDays, setHighlightedDays] = React.useState([1, 2, 15]);
-
-    const fetchHighlightedDays = (date: Dayjs) => {
-    const controller = new AbortController();
-    fakeFetch(date, {
-        signal: controller.signal,
-    })
-    .then(({ daysToHighlight }) => {
-        setHighlightedDays(daysToHighlight);
-        setIsLoading(false);
-    })
-    .catch((error) => {
-        // ignore the error if it's caused by `controller.abort`
-        if (error.name !== 'AbortError') {
-        throw error;
-        }
-    });
-        requestAbortController.current = controller;
-    };
-    React.useEffect(() => {
-        fetchHighlightedDays(initialValue);
-      // abort request on unmount
-        return () => requestAbortController.current?.abort();
-    }, []);
-    const handleMonthChange = (date: Dayjs) => {
-        if (requestAbortController.current) {
-            // make sure that you are aborting useless requests
-            // because it is possible to switch between months pretty quickly
-            requestAbortController.current.abort();
-        }
-    
-    setIsLoading(true);
-    setHighlightedDays([]);
-    fetchHighlightedDays(date);
-};
-
+    // * Open Modal 
     const [open, setOpen] = useState("");
 
     return <>
-        {/* // * Appointment body  */}
+        {/* // * Appointment body Container  */}
         <Grid item xs={8} sx={{padding:"40px"}}>
             <div style={{display:"flex", alignItems:"center"}}>
                 <div style={{flexGrow:"1"}}>
@@ -149,37 +77,23 @@ function Appointments() {
                 </Grid>
             </Grid>
         </Grid>
+        {/* //* Calendar  Container */}
         <Grid item xs={4} sx={{padding:"40px"}}>
             <Paper variant="elevation" elevation={3} sx={{padding:"1em"}}>
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DateCalendar
-                        defaultValue={initialValue}
-                        loading={isLoading}
-                        onMonthChange={handleMonthChange}
-                        renderLoading={() => <DayCalendarSkeleton />}
-                        slots={{
-                            day: ServerDay,
-                        }}
-                        slotProps={{
-                            day: {
-                                highlightedDays,
-                            } as any,
-                        }}
-                        value={value} 
-                        onChange={(newValue) => setValue(newValue)}
-                    />
-                </LocalizationProvider>
+                <TESTCalendar/>
             </Paper>
         </Grid>
-        <div>
-            <Modal
-                open={open.length > 0}
-                onClose={()=>{setOpen("")}}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Box sx={style}>
-                    {open === "add"?<>
+
+        {/* //* Modal  Container */}
+        <Modal
+            open={open.length > 0}
+            onClose={()=>{setOpen("")}}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+        >
+            <Box sx={style}>
+                {open === "add"?<>
+                    <form action="">
                         <Typography id="modal-modal-title"  variant="h5" color={"primary"} fontWeight={600} component="h2">
                             Add Appointment
                         </Typography>
@@ -228,8 +142,9 @@ function Appointments() {
                                     </MenuItem>
                                 </TextField>
                             </Grid>
-                            <Grid item sm={6} xs={12}>
+                            <Grid item xs={12}>
                                 <TextField
+                                    fullWidth
                                     label="Vehicle (Optional)"
                                     value={form.vehicle}
                                     onChange={(event) => {
@@ -237,19 +152,18 @@ function Appointments() {
                                     }}
                                 />
                             </Grid>
-                            <Grid item sm={6} xs={12}>
-                                <TextField
-                                    fullWidth
-                                    id="startTime"
-                                    label="Start Time"
-                                    variant="outlined"
-                                    type='datetime-local'
-                                    required
-                                    value={form.dateNtime}
-                                    onChange={(event) => {
-                                        setForm({...form, dateNtime: event.target.value });
-                                    }}
-                                />
+                            <Grid item xs={12}>
+                                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                    <DemoContainer components={['DateTimePicker']}>
+                                        <DateTimePicker label="Date and Time" 
+                                            slotProps={{ textField: { fullWidth: true } }}
+                                            value={form.dateTime}
+                                            onChange={(newValue) => {
+                                                setForm({...form, dateTime: dayjs(newValue)});
+                                            }}
+                                        />
+                                    </DemoContainer>
+                                </LocalizationProvider>
                             </Grid>
                             <Grid item  xs={12} mt={"4"} height={"40px"}>
                                 
@@ -266,10 +180,10 @@ function Appointments() {
                                 </Button>
                             </Grid>
                         </Grid>
-                    </>:""}
-                </Box>
-            </Modal>
-        </div>
+                    </form>
+                </>:""}
+            </Box>
+        </Modal>
     </>
     
 }
