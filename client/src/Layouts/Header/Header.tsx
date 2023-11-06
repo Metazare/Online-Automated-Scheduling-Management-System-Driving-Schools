b0c3acfdf8fcb494a628fe3be52062669445b457
Gian Carlo Dela Cruz <59326022+Deuz1007@users.noreply.github.com>
2023-11-06 22:00:59 +0800

    Update Header.tsx

--- client/src/Layouts/Header/Header.tsx
+++ client/src/Layouts/Header/Header.tsx
@@ -21,71 +21,6 @@ type Props = {}
 let badgeContentNumber = 0
 const socket = io('http://localhost:5000')
 
-// let emails[] = [];
-
-// Recieve the notification from the server
-socket.on('recieve_notification', (studentName, appointmentDate, course) => {
-  console.log(studentName, appointmentDate, course)
-
-  const emails = [studentName];
-  
-  if (studentName === '' || appointmentDate === '' || course === '') return
-  badgeContentNumber = badgeContentNumber += 1
-  
-  console.log(badgeContentNumber)
-})
-
-export interface SimpleDialogProps {
-  open: boolean;
-  selectedValue: string;
-  onClose: (value: string) => void;
-}
-
-function SimpleDialog(props: SimpleDialogProps) {
-  const { onClose, selectedValue, open } = props;
-
-  const handleClose = () => {
-    onClose(selectedValue);
-  };
-
-  const handleListItemClick = (value: string) => {
-    onClose(value);
-  };
-
-  return (
-    <Dialog onClose={handleClose} open={open}>
-      <DialogTitle>Set backup account</DialogTitle>
-      <List sx={{ pt: 0 }}>
-        {emails.map((email) => (
-          <ListItem disableGutters key={email}>
-            <ListItemButton onClick={() => handleListItemClick(email)}>
-              <ListItemAvatar>
-                <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
-                  <PersonIcon />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary={email} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-        <ListItem disableGutters>
-          <ListItemButton
-            autoFocus
-            onClick={() => handleListItemClick('addAccount')}
-          >
-            <ListItemAvatar>
-              <Avatar>
-                <AddIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary="Add account" />
-          </ListItemButton>
-        </ListItem>
-      </List>
-    </Dialog>
-  );
-}
-
 
 export default function Header({}: Props) {
 
@@ -101,16 +36,20 @@ export default function Header({}: Props) {
     setSelectedValue(value);
   };
 
+
+  // Recieve the notification from the server
+  socket.on('recieve_notification', (studentName, appointmentDate, course) => {
+    console.log(studentName, appointmentDate, course)
+    
+    if (studentName === '' || appointmentDate === '' || course === '') return
+    badgeContentNumber = badgeContentNumber += 1
+    
+    console.log(badgeContentNumber)
+  })
+
   return (
-    <div>
-      <Badge badgeContent={badgeContentNumber} color="primary">
-        <MailIcon color="action" />
-      </Badge>
-      <SimpleDialog
-        selectedValue={selectedValue}
-        open={open}
-        onClose={handleClose}
-      />
-    </div>
+    <Badge badgeContent={badgeContentNumber} color="primary">
+      <MailIcon color="action" />
+    </Badge>
   )
 }
