0402f82c7a17ae392e7c0f2bd2474f0c61240b6c
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2023-11-06 19:15:31 +0800

    fix: commented unused components on Home

--- client/src/Pages/User/Home.tsx
+++ client/src/Pages/User/Home.tsx
@@ -1,75 +1,73 @@
 import React from 'react'
 
 // * MUI IMPORTS
-import Container from '@mui/material/Container'
-import Typography from '@mui/material/Typography'
-import Avatar from '@mui/material/Avatar';
-import { Box, IconButton } from '@mui/material';
-import CallIcon from '@mui/icons-material/Call';
-import EmailIcon from '@mui/icons-material/Email';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+// import Container from '@mui/material/Container'
+// import Typography from '@mui/material/Typography'
+// import Avatar from '@mui/material/Avatar';
+// import { Box, IconButton } from '@mui/material';
+// import CallIcon from '@mui/icons-material/Call';
+// import EmailIcon from '@mui/icons-material/Email';
+// import MoreVertIcon from '@mui/icons-material/MoreVert';
+// import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
-
-type Props = {}
-
-function Home({}: Props)  {
+function Home()  {
   return (
-    <Box  sx={{ background: 'secondary.dark',width:"100vw",margin:'auto',padding:"1em"}}>
-      <h1>wew</h1>
-      {/* <Container maxWidth="lg">
-        <div style={{padding:"4rem 0",}}>
-          <a href="#" style={{display:"flex", gap:"5px",alignItems:"center", marginBottom:"10px"}}>  
-            <ArrowBackIcon/>
-            <Typography variant="subtitle1" color="initial"> Go Back</Typography>
-          </a>
-          <Box
-            sx={{
-              display: 'flex',
-              gap:"25px",
-              alignItems:"center"
-            }}
-          >
-            <Avatar
-              alt="Remy Sharp"
-              src="/static/images/avatar/1.jpg"
-              sx={{ width: 80, height: 80 }}
-            />
-            <div style={{flexGrow:"1"}}>
-              <Typography variant="h4" fontWeight={500} color="initial">SMART Driving</Typography>
-              <Box
-                sx={{
-                  display: 'flex',
-                  gap:"10px"
-                }}
-              >
-                <Box
-                sx={{
-                  display: 'flex',
-                  gap:"5px"
-                }}
-                >
-                  <CallIcon/> 
-                  <Typography variant="body1" fontWeight={500}>0915-666-147</Typography>
-                </Box>
-                <Box
-                sx={{
-                  display: 'flex',
-                  gap:"5px"
-                }}
-                >
-                  <EmailIcon/> 
-                  <Typography variant="body1" fontWeight={500}>0915-666-147</Typography>
-                </Box>
-              </Box>
-            </div>
-            <IconButton aria-label="" onClick={()=>alert}>
-              <MoreVertIcon/>
-            </IconButton>
-          </Box>
-        </div>
-      </Container> */}
-    </Box>
+    <div>Default</div>
+    // <Box  sx={{ background: 'secondary.dark',width:"100vw",margin:'auto',padding:"1em"}}>
+    //   <h1>wew</h1>
+    //   <Container maxWidth="lg">
+    //     <div style={{padding:"4rem 0",}}>
+    //       <a href="#" style={{display:"flex", gap:"5px",alignItems:"center", marginBottom:"10px"}}>  
+    //         <ArrowBackIcon/>
+    //         <Typography variant="subtitle1" color="initial"> Go Back</Typography>
+    //       </a>
+    //       <Box
+    //         sx={{
+    //           display: 'flex',
+    //           gap:"25px",
+    //           alignItems:"center"
+    //         }}
+    //       >
+    //         <Avatar
+    //           alt="Remy Sharp"
+    //           src="/static/images/avatar/1.jpg"
+    //           sx={{ width: 80, height: 80 }}
+    //         />
+    //         <div style={{flexGrow:"1"}}>
+    //           <Typography variant="h4" fontWeight={500} color="initial">SMART Driving</Typography>
+    //           <Box
+    //             sx={{
+    //               display: 'flex',
+    //               gap:"10px"
+    //             }}
+    //           >
+    //             <Box
+    //             sx={{
+    //               display: 'flex',
+    //               gap:"5px"
+    //             }}
+    //             >
+    //               <CallIcon/> 
+    //               <Typography variant="body1" fontWeight={500}>0915-666-147</Typography>
+    //             </Box>
+    //             <Box
+    //             sx={{
+    //               display: 'flex',
+    //               gap:"5px"
+    //             }}
+    //             >
+    //               <EmailIcon/> 
+    //               <Typography variant="body1" fontWeight={500}>0915-666-147</Typography>
+    //             </Box>
+    //           </Box>
+    //         </div>
+    //         <IconButton aria-label="" onClick={()=>alert}>
+    //           <MoreVertIcon/>
+    //         </IconButton>
+    //       </Box>
+    //     </div>
+    //   </Container>
+    // </Box>
   )
 }
 
