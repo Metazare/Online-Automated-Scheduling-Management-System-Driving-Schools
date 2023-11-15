9d82aee52e3aacb1e0778dc34fd37583636c7431
Harold Graphs <haroldcastillo272@gmail.com>
2023-11-15 14:39:24 +0800

    feat(school card): fix the UI for linking to the  lesson view

--- client/src/Components/SchoolCard.tsx
+++ client/src/Components/SchoolCard.tsx
@@ -4,7 +4,7 @@ import Avatar from '@mui/material/Avatar';
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
-
+import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
 type Props = {
@@ -20,11 +20,16 @@ function SchoolCard({schoolName,about, schoolId, courses, variant, courseId}:Pro
   const navigate = useNavigate();
     return (
         <div >
-              {/* {variant === "enrolled"?
-                <Paper variant="elevation" elevation={3} style={{padding:"1em 1em 3em",marginBottom:"-2.5em",background:"#2F2E5A"}}>
-                    <Typography variant="subtitle2" color="#ffffff">{courses?.length} Courses</Typography>
-                </Paper>
-              :""} */}
+            {/* {variant === "enrolled"?
+              <Paper variant="elevation" elevation={3} style={{padding:"1em 1em 3em",marginBottom:"-2.5em",background:"#2F2E5A"}}>
+                  <Typography variant="subtitle2" color="#ffffff">{courses?.length} View my Courses</Typography>
+              </Paper>
+            :""} */}
+            {variant === "enrolled"?
+              <Paper variant="elevation" elevation={3} style={{padding:"1em 1em 3em",marginBottom:"-2.5em",background:"#2F2E5A",cursor:"pointer"}}  onClick={()=>navigate(`/course/${courseId}`)}>
+                <Typography variant="subtitle2" color="#ffffff" sx={{}}  >View My Courses</Typography>
+              </Paper>
+            :""}
             <Paper variant="elevation" elevation={3} sx={{padding:"3em 1em 1em"}}>
                 <Avatar
                     alt={schoolName}
@@ -33,15 +38,9 @@ function SchoolCard({schoolName,about, schoolId, courses, variant, courseId}:Pro
                 />
                 <Typography variant="h6" mt={4} color="initial" >{schoolName}</Typography>
                 <Typography variant="body2" mt={1} color="initial">{about}</Typography>
-                {variant === "enrolled" ?
-                  <Button variant="contained" sx={{mt:"30px",width:"100%"}} color="primary" onClick={()=>navigate(`/course/${courseId}`)}>
-                      view
-                  </Button>
-                :
-                  <Button variant="contained" sx={{mt:"30px",width:"100%"}} color="primary" onClick={()=>navigate(`/school/${schoolId}`)}>
-                      view
-                  </Button>
-                }
+                <Button variant="contained" sx={{mt:"30px",width:"100%"}} color="primary" onClick={()=>navigate(`/school/${schoolId}`)}>
+                  view
+                </Button>
             </Paper>
         </div>
     )
