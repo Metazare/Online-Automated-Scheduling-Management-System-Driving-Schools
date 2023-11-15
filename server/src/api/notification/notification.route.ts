cff94ef965c0618f6268efc190136595aa96eba5
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2023-11-15 11:48:49 +0800

    feat(notif): add read notification route on server

--- server/src/api/notification/notification.route.ts
+++ server/src/api/notification/notification.route.ts
@@ -1,9 +1,11 @@
-import { getNotifications } from "./notification.controller";
+import { getNotifications, readNotification } from "./notification.controller";
 import { Router } from "express";
 import asynchronousHandler from "../../middlewares/asynchronousHandler";
 
 const router: Router = Router();
 
 router.get('/', asynchronousHandler(getNotifications));
+
+router.patch('/', asynchronousHandler(readNotification));
 
 export default router;
