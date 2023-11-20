fdd9c69b4bbbac4b7de97f713c953344632f12bf
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2023-11-20 19:23:27 +0800

    fix: post request on send email

--- client/src/Test/SendEmail.tsx
+++ client/src/Test/SendEmail.tsx
@@ -1,11 +1,12 @@
 import { SlackConfirmEmail } from './emailTemplate';
 // import { Resend } from 'resend';
 
-import React from 'react';
+import React, { useState } from 'react';
 // import { Button } from '@react-email/components';
 import { FormLabel  } from "@mui/material"
 import { Input } from '@mui/material';
-import axios from 'axios';
+// import axios from 'axios';
+import axios from './../Hooks/useAxios'
 
 // const resend = new Resend(process.env.RESEND_KEY);
 
@@ -19,6 +20,10 @@ interface sendingEmail {
 const dateAsString = '2023-11-10T12:00:00Z';
 
 const SendEmails = () => {
+  const [form, setForm] = useState({
+    email: '',
+    otp: ''
+  })
 
     async function handleOnSubmit(e: React.SyntheticEvent<HTMLFormElement>) {
         e.preventDefault();
@@ -27,14 +32,16 @@ const SendEmails = () => {
         try{
             await axios
             .post('/otp', {
-                from: 'v0h7H@example.com',
-                to: 'deuzaxel@gmail.com',
-                subject: 'Slack Confirm Email',
-                react: SlackConfirmEmail({
-                    reservationNumber: '12345',
-                    reservationDate: new Date(dateAsString),
-                    reservationStatus: 'Pending',
-                })
+                // from: 'v0h7H@example.com',
+                // to: 'deuzaxel@gmail.com',
+                // subject: 'Slack Confirm Email',
+                // react: SlackConfirmEmail({
+                //     reservationNumber: '12345',
+                //     reservationDate: new Date(dateAsString),
+                //     reservationStatus: 'Pending',
+                // })
+                email: form.email,
+                code: form.otp
             })
             .then((response:any)=>{
                 console.log(response.data);
@@ -47,10 +54,10 @@ const SendEmails = () => {
 
     return (
         <form className="flex flex-col gap-4" onSubmit={handleOnSubmit}>
-            <FormLabel htmlFor="firstName">First Name</FormLabel>
-            <Input id="firstName" name="firstName" />
+            <FormLabel htmlFor="firstName">OTP</FormLabel>
+            <Input id="otp" name="otp" onChange={(e) => setForm({...form, otp: e.target.value})}/>
             <FormLabel htmlFor="email">Email</FormLabel>
-            <Input id="email" name="email" />
+            <Input id="email" name="email" onChange={(e) => setForm({...form, email: e.target.value})}/>
             <button
                 className={"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"}
                 type="submit"
