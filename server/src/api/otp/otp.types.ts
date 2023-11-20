d1c053d3a99556860dca6ff76d105f4a9d3fab6d
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2023-11-20 19:22:53 +0800

    chore: code cleanup

--- server/src/api/otp/otp.types.ts
+++ server/src/api/otp/otp.types.ts
@@ -1,9 +1,4 @@
 export interface OTP {
     email: string;
-    code: number;
-}
-
-export interface OTPDocument extends OTP, Document {
-    createdAt: Date;
-    updatedAt: Date;
+    code: string;
 }
