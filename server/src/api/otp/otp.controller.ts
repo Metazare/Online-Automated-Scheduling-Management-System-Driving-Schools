1cd4bb533cd0f86ced438d75d3378250769b7902
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2023-11-20 19:22:45 +0800

    fix: sending of email at otp controller

--- server/src/api/otp/otp.controller.ts
+++ server/src/api/otp/otp.controller.ts
@@ -4,7 +4,6 @@ import { OTP } from './otp.types';
 import { Resend } from 'resend';
 import { UnprocessableEntity } from '../../utilities/errors';
 import envs from '../../utilities/envs';
-import otpModel from './otp.model';
 
 const { RESEND_KEY } = envs;
 const resend = new Resend(RESEND_KEY);
@@ -13,17 +12,13 @@ export const createOTP: RequestHandler = async (req: BodyRequest<OTP>, res) => {
     const { email, code } = req.body;
 
     const checker = new CheckData();
-    checker.checkType(email, 'string', 'email');
-    checker.checkType(code, 'number', 'code');
-
     if (checker.size()) throw new UnprocessableEntity(checker.errors);
 
-    await otpModel.findOneAndUpdate({ email }, { code }, { upsert: true });
-    await resend.emails.send({
-        from: 'verification@oasms.dev',
+    resend.emails.send({
+        from: 'onboarding@resend.dev',
         to: email,
         subject: 'Email Verification',
-        react: `This is your code: <b>${code}</b>`
+        html: `Your One Time Password (OTP) is: <b>${code}</b>`
     })
 
     res.sendStatus(201);
