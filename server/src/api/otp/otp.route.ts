7eb160f1b570c9d7a33b21793446953fe3ab4dab
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2023-11-21 23:23:11 +0800

    refactor: remove old otp files

--- server/src/api/otp/otp.route.ts
+++
@@ -1,9 +0,0 @@
-import { createOTP } from './otp.controller';
-import { Router } from 'express';
-import asynchronousHandler from '../../middlewares/asynchronousHandler';
-
-const router: Router = Router();
-
-router.post('/', asynchronousHandler(createOTP));
-
-export default router;
--- server/src/api/otp/otp.types.ts
+++
@@ -1,4 +0,0 @@
-export interface OTP {
-    email: string;
-    content: string;
-}
