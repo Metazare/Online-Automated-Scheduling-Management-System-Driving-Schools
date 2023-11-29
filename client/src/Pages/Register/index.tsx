6099311e5b936777702139ac226b5fa4003cd7a7
Harold Graphs <haroldcastillo272@gmail.com>
2023-11-30 03:31:14 +0800

    fix:favicon logo

--- client/src/Pages/Register/index.tsx
+++ client/src/Pages/Register/index.tsx
@@ -429,7 +429,7 @@ function Index() {
                 </Grid>
 
 
-                <Grid item xs={9}>
+                <Grid item xs={12}>
                   <TextField
                     fullWidth
                     id="email"
