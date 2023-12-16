19d3094f6b40a28b5ed0d1174e827e100f6829aa
Meeep <139204086+Meeepu@users.noreply.github.com>
2023-12-17 07:47:18 +0800

    style(email): prettier auto format

--- server/src/api/email/email.controller.ts
+++ server/src/api/email/email.controller.ts
@@ -6,32 +6,29 @@ const nodemailer = require('nodemailer');
 const { NODEMAILER_EMAIL, NODEMAILER_PASSWORD } = envs;
 
 export const sendEmail: RequestHandler = async (req: BodyRequest<Email>, res) => {
-  const { to, subject, content } = req.body;
+    const { to, subject, content } = req.body;
 
-  const transporter = nodemailer.createTransport({
-    service: 'outlook',
-    auth: {
-      user: NODEMAILER_EMAIL,
-      pass: NODEMAILER_PASSWORD,
-    },
-  });
-
-  try {
-
-    // Send email
-    const info = await transporter.sendMail({
-      from: NODEMAILER_EMAIL, // Sender address
-      to: to, // Recipient address
-      subject: subject, // Subject line
-      html: content // HTML body
+    const transporter = nodemailer.createTransport({
+        service: 'outlook',
+        auth: {
+            user: NODEMAILER_EMAIL,
+            pass: NODEMAILER_PASSWORD
+        }
     });
 
-    res.json(info);
-  } catch (error: any) {
-    console.error('Error sending email: ', error.message);
-    // General error response
-    res.status(500).json({ error: 'Error sending email', message: error.message });
-  }
+    try {
+        // Send email
+        const info = await transporter.sendMail({
+            from: NODEMAILER_EMAIL, // Sender address
+            to: to, // Recipient address
+            subject: subject, // Subject line
+            html: content // HTML body
+        });
 
+        res.json(info);
+    } catch (error: any) {
+        console.error('Error sending email: ', error.message);
+        // General error response
+        res.status(500).json({ error: 'Error sending email', message: error.message });
+    }
 };
-
